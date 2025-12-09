4c170b364682b59ae4f1f1fe6d170f0ce895dfd3
tachothai524 <tachothai_th@cmu.ac.th>
2025-12-09 16:54:36 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    int x = -1;
+    int i = 0;
+    int j = 0;
+    while(x != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x ;
+    
+    if(x % 2 == 0){
+        i++ ;
+    }
+    else{
+        j++ ;
+    }
+    
+    if(x == 0){
+        i = i - 1;
+    }
+    
+}
+    cout << "#Even numbers = " << i << "\n" ;
+    cout << "#Odd numbers = " << j ;
     return 0;
 }
